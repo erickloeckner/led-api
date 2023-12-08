ebc1f648c5cb15f459819943c776736610441165
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2023-12-08 04:12:47 -0500

    Create generate_service_file.sh

---
+++ generate_service_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dir=$(pwd)
+user=$(whoami)
+echo "[Unit]\nDescription=LED API\n\n[Service]\nExecStart=$dir/target/release/led-api $dir/config.toml\nUser=$user\nWorkingDirectory=$dir\n\n[Install]\nWantedBy=multi-user.target" > ./led-api.service
