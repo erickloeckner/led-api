2dc88eb03c0ec507326f63db8a63b2ade5b73e6f
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2024-07-17 21:12:21 -0400

    Update generate_service_file.sh
    added -e flag to echo to interpret newlines

--- generate_service_file.sh
+++ generate_service_file.sh
@@ -2,4 +2,4 @@
 
 dir=$(pwd)
 user=$(whoami)
-echo "[Unit]\nDescription=LED API\n\n[Service]\nExecStart=$dir/target/release/led-api $dir/config.toml\nUser=$user\nWorkingDirectory=$dir\n\n[Install]\nWantedBy=multi-user.target" > ./led-api.service
+echo -e "[Unit]\nDescription=LED API\n\n[Service]\nExecStart=$dir/target/release/led-api $dir/config.toml\nUser=$user\nWorkingDirectory=$dir\n\n[Install]\nWantedBy=multi-user.target" > ./led-api.service
